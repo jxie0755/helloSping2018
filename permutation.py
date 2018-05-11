5d4865a2fc1cb87d91a8f83b92e2909797920265
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-11 08:35:54 -0400

    答案

--- permutation.py
+++ permutation.py
@@ -15,3 +15,28 @@ if __name__ == '__main__':
     assert permutations(['angie', 'cat']) == [['angie', 'cat'], ['cat', 'angie']]
     assert permutations([1, 2, 3]) == [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
     print('all passed')
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # 答案
+def permutations(lst):
+    """List all permutations of the given list"""
+    ### Your code here ###
+    if len(lst) <= 1:
+        return [lst]
+    total = []
+    for i, k in enumerate(lst):
+        total.extend([[k] + p for p in permutations(lst[:i] + lst[i+1:])])
+    return total
