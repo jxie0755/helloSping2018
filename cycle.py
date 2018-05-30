e221ab3c332d739b08e520ac8ca80ef20fbe9645
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-30 01:35:25 -0400

    Update cycle.py

--- cycle.py
+++ cycle.py
@@ -2,19 +2,19 @@ def cycle(f1, f2, f3):
     """Returns a function that is itself a higher-order function.
 
     f1, f2, f3 各为一个函数
-    返回一个高阶函数FF, 自带一个参数n,使得FF(n):
+    返回一个高阶函数FF, 自带一个参数n,使得FF(n)随着n的变化,成为不同的函数:
 
-    若n == 0, 则return lambda x: x
+    若n == 0, 则 GG = FF(0), 使得GG(x) return x
 
-    若n == 1, 则return lambda x: f1(x)
-    若n == 2, 则return lambda x: f2(f1(x))
-    若n == 3, 则return lambda x: f3(f2(f1(x))
+    若n == 1, 则 GG = FF(1), 使得GG(x) return f1(x)
+    若n == 2, 则 GG = FF(2), 使得GG(x) return f2(f1(x))
+    若n == 3, 则 GG = FF(3), 使得GG(x) return f3(f2(f1(x))
 
-    若n == 4, 则return lambda x: f1(f3(f2(f1(x))))
-    若n == 5, 则return lambda x: f2(f1(f3(f2(f1(x)))))
-    若n == 6, 则return lambda x: f3(f2(f1(f3(f2(f1(x))))))
+    若n == 4, 则 GG = FF(4), 使得GG(x) return f1(f3(f2(f1(x))))
+    若n == 5, 则 GG = FF(5), 使得GG(x) return f2(f1(f3(f2(f1(x)))))
+    若n == 6, 则 GG = FF(6), 使得GG(x) return f3(f2(f1(f3(f2(f1(x))))))
 
-    若n == 7, 则return lambda n: f1(f3(f2(f1(f3(f2(f1(x)))))))
+    若n == 7, 则 GG = FF(7), 使得GG(x) return f1(f3(f2(f1(f3(f2(f1(x)))))))
     ...
     ...
     无限叠加
@@ -35,22 +35,22 @@ if __name__ == '__main__':
     def add3(x):
         return x + 3
 
-    TTest = cycle(add1, times2, add3)
+    FFtest = cycle(add1, times2, add3)
 
-    identity = TTest(0)
-    assert identity(0) == 0, 'base test 1'
-    assert identity(2) == 2, 'base test 2'
+    GGtest = FFtest(0)
+    assert GG(0) == 0, 'base test 1'
+    assert GG(2) == 2, 'base test 2'
 
-    G =  TTest(2)
-    assert G(1) == 4, 'f2(f1(n)), (1+1)*2 = 4'
+    GGtest =  FFtest(2)
+    assert GGtest(1) == 4, 'f2(f1(n)), (1+1)*2 = 4'
 
-    H = TTest(3)
-    assert H(2) == 9, 'f3(f2(f1(3))), ((2+1)*2)+3 = 9'
+    GGtest = FFtest(3)
+    assert GGtest(2) == 9, 'f3(f2(f1(3))), ((2+1)*2)+3 = 9'
 
-    J = TTest(4)
-    assert J(2) == 10, 'f1(f3(f2(f1(3)))), (((2+1)*2)+3)+1 = 10'
+    GGtest = FFtest(4)
+    assert GGtest(2) == 10, 'f1(f3(f2(f1(3)))), (((2+1)*2)+3)+1 = 10'
 
-    K = TTest(6)
-    assert K(1) == 19, 'f3(f2(f1(f3(f2(f1(1)))))), (((1+1)*2)+3)+1)*2)+3 = 19'
+    GGtest = FFtest(6)
+    assert GGtest(1) == 19, 'f3(f2(f1(f3(f2(f1(1)))))), (((1+1)*2)+3)+1)*2)+3 = 19'
     
     print('all passed')
