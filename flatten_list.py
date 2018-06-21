1eee449e6c9538b5ba77654ebdf0d9630fabf9a2
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-21 11:04:49 -0400

    Create flatten_list.py

---
+++ flatten_list.py
@@ -0,0 +1,15 @@
+# 清除嵌套的list
+
+def flat_list(nested_list):
+    """把嵌套的list变成一个简单的list"""
+    # input your code:
+    
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
+
